902185aa614ecdc1245ae86284771b365b3a434a
asiakay <asialakaygrady@gmail.com>
2020-10-22 09:59:17 -0400

    1 file modified, 1 added - moved Counter component and the action creator to a separate file named Counter.js

--- index.js
+++ index.js
@@ -2,26 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-
+import Counter from './Counter';
 import './style.css';
 
-class Counter extends React.Component {
-  state = {
-    counter: 0
-  }
-  increment = () => {
-    this.setState({counter: this.state.counter+1});
-  }
-  componentDidUpdate() {
-    alert("Number of clicks: " + this.state.counter);
-  }
-  render() {
-    return <div>
-    <p>{this.state.counter}</p>
-    <button onClick={this.increment}>Increment</button>
-    </div>;
-  }
-}
+const initialState = {}
 // create an action and correspondig reducer
 function incrementCounter(num){
   return {
@@ -29,23 +13,17 @@ function incrementCounter(num){
   num: num
   }
 }
-
-//our reducer.
-const initialState={
-  count: 0
-};
-
+// Reducer Function
 function reducer(state = initialState, action) {
   switch(action.type){
-    case 'Increment':
-      return {count: state + action.num};
+    case 'INCREMENT':
+      return {count: state.count + action.num};
     default: 
       return state;
   }
 }
 
 const store = createStore(reducer);
-
 const el = <Provider store={store}>
   <Counter />
   </ Provider>; 
